5554d02cac00a3225135963087fe673abf4f6132
Sun-52 <102531810+Sun-52@users.noreply.github.com>
2022-10-19 15:08:16 +0700

    chnage method for viewing post

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
   app.route("/post/search").get(taskBuilder.search); //finished
   app
     .route("/post/:post_id")
-    .get(taskBuilder.view_post) //finished
+    .patch(taskBuilder.view_post) //finished
     .post(taskBuilder.post_comment); //finished
   app.route("/post/:post_id/:user_id").patch(taskBuilder.like); //finished
 };
