390aff47e9f4e23e7421be21b7a5c8b4a81ee5c3
Sun-52 <102531810+Sun-52@users.noreply.github.com>
2022-11-11 20:41:42 +0700

    Start https

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ global.comment = require("./api/models/commentModel");
 const userRoutes = require("./api/routes/userRoutes");
 const postRoutes = require("./api/routes/postRoutes");
 const uploadRoutes = require("./api/routes/uploadRoutes");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+var privateKey = fs.readFileSync("key.pem", "utf8");
+var certificate = fs.readFileSync("cert.pem", "utf8");
+
+var credentials = { key: privateKey, cert: certificate };
 
 mongoose.connect(
   "mongodb+srv://Sun_Seree:Su214221@micro.zynfiw9.mongodb.net/Micro_main",
@@ -17,6 +24,12 @@ mongoose.connect(
 
 const port = process.env.PORT || 4500;
 const app = express();
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(port);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +40,7 @@ userRoutes(app);
 postRoutes(app);
 uploadRoutes(app);
 
-app.listen(port);
+//app.listen(port);
 
 app.use((req, res) => {
   res.status(404).send({ url: `${req.originalUrl} not found` });
